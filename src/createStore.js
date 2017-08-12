3ffbcb696880ec6adadbd3b90c2d0f21ff61d05a
Naz Khan <nazmuskhan@yahoo.com>
2017-08-11 23:05:13 -0400

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,1 +1,21 @@
+export default function createStore(reducer){
+  let state;
 
+  function getState(){
+    return state
+  }
+
+  function dispatcher(action){
+    state = reducer(state, action)
+    render()
+  }
+
+  return {
+    dispatch,
+    getState
+  }
+}
+
+function render(){
+
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import changeCount from './reducers/changeCount'
+import createStore from './createStore'
+
+const store = createStore(changeCount)
 
 export function render() {
   ReactDOM.render(
@@ -9,4 +13,4 @@ export function render() {
   );
 };
 
-render();
+store.dispatch({ type: '@@INIT' });
